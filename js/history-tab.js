8b7f2343d568ef9a6b3849de9a380b0bde02f1f0
shahrup-shihab <muhammodshihab2@gmail.com>
2024-10-03 20:40:58 +0600

    updated

--- js/history-tab.js
+++ js/history-tab.js
@@ -2,12 +2,12 @@ const btnHistory = document.getElementById('history-btn')
 const btnDonation = document.getElementById('donation-btn')
 btnHistory.addEventListener('click', function () {
 
-    btnHistory.classList.add('hover:bg-[#B4F461]', 'bg-[#B4F461]', 'font-bold')
+    btnHistory.classList.add('hover:bg-[#B4F461]', 'bg-[#B4F461]', 'font-bold');
 
-    btnDonation.classList.remove('hover:bg-[#B4F461]', 'bg-[#B4F461]', 'font-bold')
+    btnDonation.classList.remove('hover:bg-[#B4F461]', 'bg-[#B4F461]', 'font-bold');
 
-    document.getElementById('card-box').classList.add('hidden')
-    document.getElementById('history-section').classList.remove('hidden')
+    document.getElementById('card-box').classList.add('hidden');
+    document.getElementById('history-section').classList.remove('hidden');
 
 
 });
@@ -16,11 +16,11 @@ const btnHistoryTab = document.getElementById('history-btn')
 const btnDonationTab = document.getElementById('donation-btn')
 btnDonationTab.addEventListener('click', function () {
 
-    btnDonationTab.classList.add('hover:bg-[#B4F461]', 'bg-[#B4F461]', 'font-bold')
+    btnDonationTab.classList.add('hover:bg-[#B4F461]', 'bg-[#B4F461]', 'font-bold');
 
-    btnHistoryTab.classList.remove('hover:bg-[#B4F461]', 'bg-[#B4F461]', 'font-bold')
+    btnHistoryTab.classList.remove('hover:bg-[#B4F461]', 'bg-[#B4F461]', 'font-bold');
 
-    document.getElementById('card-box').classList.remove('hidden')
-    document.getElementById('history-section').classList.add('hidden')
+    document.getElementById('card-box').classList.remove('hidden');
+    document.getElementById('history-section').classList.add('hidden');
 
 });
