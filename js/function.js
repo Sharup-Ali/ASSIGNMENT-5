168a98795bad7f6dd5804d4e15dc4de87262d02c
shahrup-shihab <muhammodshihab2@gmail.com>
2024-10-03 20:35:19 +0600

    function js

--- js/function.js
+++ js/function.js
@@ -1,26 +1,13 @@
-const btnHistory = document.getElementById('history-btn')
-const btnDonation = document.getElementById('donation-btn')
-btnHistory.addEventListener('click', function () {
+function getInputFieldValueById(id){
+    const inputValue = document.getElementById(id).value;
+    const inputNumber = parseFloat(inputValue);
+    // console.log(id, inputValue, inputNumber)
+    return inputNumber
+}
 
-    btnHistory.classList.add('hover:bg-[#B4F461]', 'bg-[#B4F461]', 'font-bold')
+function getTextFieldValueById(id){
+    const textValue = document.getElementById(id).innerText;
+    const textNumber = parseFloat(textValue);
+    return textNumber
+}
 
-    btnDonation.classList.remove('hover:bg-[#B4F461]', 'bg-[#B4F461]', 'font-bold')
-
-    document.getElementById('card-box').classList.add('hidden')
-    document.getElementById('history-section').classList.remove('hidden')
-
-
-});
-
-const btnHistoryTab = document.getElementById('history-btn')
-const btnDonationTab = document.getElementById('donation-btn')
-btnDonationTab.addEventListener('click', function () {
-
-    btnDonationTab.classList.add('hover:bg-[#B4F461]', 'bg-[#B4F461]', 'font-bold')
-
-    btnHistoryTab.classList.remove('hover:bg-[#B4F461]', 'bg-[#B4F461]', 'font-bold')
-
-    document.getElementById('card-box').classList.remove('hidden')
-    document.getElementById('history-section').classList.add('hidden')
-
-});
